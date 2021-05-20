63ab0cd46b6a0d31263373f75c28ffec17f3b873
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-20 23:27:11 +0600

    signup template

---
+++ invoicevue/src/views/dashboard/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page-dashboard">
+        <div class="columns is-multline">
+            <div class="column is-12">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+    name: 'Dashboard'
+}
+</script>
