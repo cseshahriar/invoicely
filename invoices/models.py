f625af5d13ede924f156b4d39bc652e60bd7aa86
cseshahriar <cse.shahriar.hosen@gmail.com>
2021-05-26 17:09:30 +0600

    invoice item model

--- invoices/models.py
+++ invoices/models.py
@@ -42,3 +42,16 @@ class Invoice(models.Model):
 
     def __str__(self):
         return str(self.invoice_number)
+
+
+class Item(models.Model):
+    invoice = models.ForeignKey(Invoice, related_name='items', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    quantity = models.IntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    net_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    vat_rate = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
